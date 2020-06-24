2b85755ed5ae11d1b44a0935977484c3b46a0f9e
Jacob E <yaakovepstein@gmail.com>
2020-06-24 14:47:06 -0400

    changed cred

--- src/OrderPage.js
+++ src/OrderPage.js
@@ -75,9 +75,28 @@ class OrderPage extends React.Component {
  );
 }
 
+log=() =>{
+    const winston = require('winston');
+    const logger = winston.createLogger({
+        level: 'info',
+        format: winston.format.json(),
+        defaultMeta: { service: 'user-service' },
+        transports: [
+          //
+          // - Write to all logs with level `info` and below to `combined.log` 
+          // - Write all logs error (and below) to `error.log`.
+          //
+         
+          new winston.transports.File({ filename: 'error.log', level: 'error' }),
+          new winston.transports.File({ filename: 'combined.log' })
+        ]
+      }); 
+      logger.log("hi"); 
+     
+}
 orderFetch=() =>
 {
-    
+  //  this.log();
  
   this.props.foo(this.state)
 }
