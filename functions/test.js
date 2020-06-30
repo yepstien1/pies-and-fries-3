46664798e2f5cd6f012ba8728dcd958b4aa38918
Jacob E <yaakovepstein@gmail.com>
2020-06-29 21:53:40 -0400

    practice netlify functions

--- functions/test.js
+++ functions/test.js
@@ -1,4 +1,8 @@
+const axios = require('axios')
 exports.handler = function(event,context,callback){
+    const GOOGLE_API_KEY="FILL";
+    
+    const CLIENT_ID="FILL";
     callback(null,{
         statusCode:200,
         body: 'yay',
