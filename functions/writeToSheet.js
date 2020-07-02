6695b59f61f32589452b90563fb376c19209ed6e
Jacob E <yaakovepstein@gmail.com>
2020-07-02 13:00:21 -0400

    trying something out

--- functions/writeToSheet.js
+++ functions/writeToSheet.js
@@ -1,8 +1,55 @@
-const bob = process.env
-exports.handler =async event =>{
+
+
+
+const bob = process.env;
+const gapi =require('googleapis');
+
+exports.handler =async (event,orderInfo )=>{
+
+ 
+    gapi.initClient=()=>{
+        gapi.client.init({
+             'apiKey': bob.REACT_APP_GOOGLE_API_KEY,
+             'clientId': bob.REACT_APP_CLIENT_ID,
+             'scope': "https://www.googleapis.com/auth/spreadsheets",
+             'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+         }).then(()=> {
+             gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus); //add a function called `updateSignInStatus` if you want to do something once a user is logged in with Google
+            this.updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+ 
+ //authorizeButton.onclick = this.handleAuthClick;
+            //signoutButton.onclick = this.handleSignoutClick;
+ 
+              });
+
+
+         
     return {
         statusCode:200,
         body: bob.REACT_APP_GOOGLESHEET_ID
 
     }
+    }
+
+    
+  function  updateSigninStatus (isSignedIn){
+
+    if (isSignedIn) {
+       // authorizeButton.style.display = 'none';
+       // signoutButton.style.display = 'block';
+
+         // makeApiCall();
+     } else {
+       //  authorizeButton.style.display = 'block';
+        // signoutButton.style.display = 'none';
+         handleAuthClick();
+     }
+
+
+
+ }
+}
+function handleAuthClick (event){
+    
+    gapi.auth2.getAuthInstance().signIn();
 }
