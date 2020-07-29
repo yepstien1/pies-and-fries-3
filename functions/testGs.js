161bbb5e535ef65ef640c09f4a472b65253d976b
Jacob E <yaakovepstein@gmail.com>
2020-07-28 21:26:39 -0400

    ks;;ssks's

--- functions/testGs.js
+++ functions/testGs.js
@@ -24,10 +24,10 @@ exports.handler = async=>{
      * @param {function} callback The callback to call with the authorized client.
      */
     function authorize(credentials, callback) {
-      const {client_secret, client_id, redirect_uris} = credentials;
-      console.log(redirect_uris[0]);
+      const {client_secret, client_id} = credentials;
+     // console.log(redirect_uris[0]);
       const oAuth2Client = new google.auth.OAuth2(
-          client_id, client_secret, redirect_uris[0]);
+          client_id, client_secret);
     
       // Check if we have previously stored a token.
       fs.readFile(TOKEN_PATH, (err, token) => {
