0218f4d99bb77634806af16bfc59b4573ef19a04
Jacob E <yaakovepstein@gmail.com>
2020-07-28 21:41:26 -0400

    switched env vairable stuff

--- functions/testGs.js
+++ functions/testGs.js
@@ -24,10 +24,10 @@ exports.handler = async=>{
      * @param {function} callback The callback to call with the authorized client.
      */
     function authorize(credentials, callback) {
-      const {client_secret, client_id} = credentials;
+      const {client_secret, client_id,redirect_uris} = credentials.installed;
      // console.log(redirect_uris[0]);
       const oAuth2Client = new google.auth.OAuth2(
-          client_id, client_secret);
+          client_id, client_secret,redirect_uris[0]);
     
       // Check if we have previously stored a token.
       fs.readFile(TOKEN_PATH, (err, token) => {
