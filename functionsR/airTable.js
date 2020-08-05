f6629da69847d88c10490b0e57c24a7dd79e5d64
Jacob E <yaakovepstein@gmail.com>
2020-08-05 16:20:45 -0400

    figgured out how to send. seeing how to acesss

--- functionsR/airTable.js
+++ functionsR/airTable.js
@@ -1,11 +1,14 @@
+const { createSemicolonClassElement } = require('typescript');
+
 exports.handler=async (event,context)=>{
 console.log(event);
 console.log(context);
 
     var Airtable = require('airtable');
     var base = new Airtable({apiKey: process.env.REACT_APP_AIR_TABLE_API_KEY}).base('appbTi7sjlAEMnht7');
-    
-    
+    console.log("event"+event);
+    console.log("context"+context);
+    console.log("body" +context.body)
 base('Table 1').create([
     {
       "fields": {
