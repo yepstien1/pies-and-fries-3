93cf06674e0eaadc382960402f90e73b1fda4b17
Jacob E <yaakovepstein@gmail.com>
2020-08-11 12:52:14 -0400

    swithcing to node.fetch

--- functionsR/airTable.js
+++ functionsR/airTable.js
@@ -13,7 +13,7 @@ console.log(context);
   var info=/*JSON.parse(event.body)||*/{"name":"fakedata"};
     console.log("body" +event.body)
     console.log(info.name);
-    const airTablePromise=
+ /*   const airTablePromise=
 base('Table 1').create([
     {
       "fields": {
@@ -39,4 +39,29 @@ base('Table 1').create([
   console.error(err)
 }) 
 console.log("hi from end of func")
+*/
+
+
+
+
+const fetch = require("node-fetch")
+data = {"records":[{"id":"rec6fKsp9dcLh2UwZ","fields":{"Name":"bomb","Email":"bob@bob.com","Total Fries Cost":23}},{"id":"recL6wFUPMUiYgq9v","fields":{"Name":"howdy","Email":"howdy.com","Total Fries Cost":43}}]})
+const neder =fetch('https://api.airtable.com/v0/appbTi7sjlAEMnht7/Table%201', {
+        method: 'patch',
+        body:    JSON.stringify(data),
+		'Authorization' : 'Bearer  ${process.env.REACT_APP_AIR_TABLE_API_KEY}',
+        headers: { 'Content-Type': 'application/json' },
+    })
+    .then(res => res.json())
+    .then(json => console.log(json));
+	console.log("Request issued", neder)
+
+   neder.then(records => {
+  console.info("Worked!", records)
+  records.forEach(x => console.log(x.getId()))
+}, err => {
+  console.error(err)
+}) 
+
+
 }
