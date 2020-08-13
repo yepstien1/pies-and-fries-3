56278ecc42fb7547f39a85e07dc015054592a5a0
Jacob E <yaakovepstein@gmail.com>
2020-08-13 15:34:36 -0400

    fixed other error

--- src/Review.js
+++ src/Review.js
@@ -244,29 +244,12 @@ fetch("https://pies-and-fries.netlify.app/.netlify/functions/sendEmail", {
   console.error('Error:', error);
 });
 
-/*this.sendEmail(abbreviatedState);
 
-
-        }
-        sendEmail= (bob)=>{
-
-     
-        emailjs.send('gmail','template_kmmjAIKP',bob, process.env.REACT_APP_EMAILJS_ID).then(res => {
-                                                      	console.log('Email successfully sent!')
-                                                    	})
-                                                    	// Handle errors here however you like, or use a React error boundary
-                                                        .catch(err => console.error('Oh well, you failed. Here some thoughts on the error that occured:', err))
-                                                        
-                                                        
-                                                      
-
-                                                        
-                                                    }
-                                                    */
 
 
                                                       // bring up confirmation page
                                                       this.props.foo();
+}
 returnToOrderPage =() =>{
     this.props.foo('ordered');
 }
