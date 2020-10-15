5f9247e9a15fa5ac423dd55bff104cae3c5b861c
Jacob E <yaakovepstein@gmail.com>
2020-10-15 17:39:39 -0400

    added some  more stripe stuff

--- src/PaymentPage.js
+++ src/PaymentPage.js
@@ -7,8 +7,31 @@ import {loadStripe} from '@stripe/stripe-js';
 const stripePromise = loadStripe('pk_test_51HIfiHBNlDExaBq3QpRDRJ7R1RFLJ3r5TrYDKUk34iq8gY1sXd5Jyh2OSGXpNEgNcsgTC3qBhkXjiKQ9LfYaBxpt00a6dtcUiR');
 
 function PayMentPage() {
+
+    const handleClick = async (event) => {
+        // Get Stripe.js instance
+        const stripe = await stripePromise;
+
+        // Call your backend to create the Checkout Session
+        const response = await fetch('/create-checkout-session', {method: 'POST'});
+
+        const session = await response.json();
+
+        // When the customer clicks on the button, redirect them to Checkout.
+        const result = await stripe.redirectToCheckout({
+            sessionId: session.id,
+        });
+
+        if (result.error) {
+            // If `redirectToCheckout` fails due to a browser or network
+            // error, display the localized error message to your customer
+            // using `result.error.message`.
+        }
+    };
+
+
     return (
-        <button role="link">
+        <button role="link" onClick={handleClick}>
             Checkout
         </button>
     );
