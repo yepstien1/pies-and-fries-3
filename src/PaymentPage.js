8649fae07da660b950b5a74e2060f805edf3ce35
Jacob E <yaakovepstein@gmail.com>
2020-10-15 17:35:59 -0400

    added some stripe stuff

--- src/PaymentPage.js
+++ src/PaymentPage.js
@@ -1,140 +1,19 @@
-import React,{ useState, useEffect } from "react";
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js"
-import {
-    CardElement,
-    useStripe,
-    useElements
-  } from "@stripe/react-stripe-js";
-//import "./App.css";
-// Make sure to call loadStripe outside of a component’s render to avoid
-// recreating the Stripe object on every render.
-// loadStripe is initialized with your real test publishable API key.
-const stripeKey=process.env.REACT_APP_STRIPE;
-const promise = loadStripe(stripeKey);
-export default function PaymentPage() {
-  return (
-    <div className="PaymentPage">
-      <Elements stripe={promise}>
-        <CheckoutForm />
-      </Elements>
-    </div>
-  );
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Confirmation from "./Confirmation";
+import {loadStripe} from '@stripe/stripe-js';
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe('pk_test_51HIfiHBNlDExaBq3QpRDRJ7R1RFLJ3r5TrYDKUk34iq8gY1sXd5Jyh2OSGXpNEgNcsgTC3qBhkXjiKQ9LfYaBxpt00a6dtcUiR');
+
+function PayMentPage() {
+    return (
+        <button role="link">
+            Checkout
+        </button>
+    );
 }
 
-
- function CheckoutForm() {
-    const [succeeded, setSucceeded] = useState(false);
-    const [error, setError] = useState(null);
-    const [processing, setProcessing] = useState('');
-    const [disabled, setDisabled] = useState(true);
-    const [clientSecret, setClientSecret] = useState('');
-    const stripe = useStripe();
-    const elements = useElements();
-    useEffect(() => {
-      // Create PaymentIntent as soon as the page loads
-      
-
-      window
-        .fetch("https://pies-and-fries.netlify.app/.netlify/functions/acceptPayment", {
-          method: "POST",
-          mode: "no-cors",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify({items: [{ id: "xl-tshirt" }]})
-        })
-        .then(res => {
-
-          return res.json();
-        })
-        .then(data => {
-          setClientSecret(data.clientSecret);
-        },err =>{
-        
-          console.log("error:" +err)
-        });
-
- 
-    }, []);
-    
-    const cardStyle = {
-      style: {
-        base: {
-          color: "#32325d",
-          fontFamily: 'Arial, sans-serif',
-          fontSmoothing: "antialiased",
-          fontSize: "16px",
-          "::placeholder": {
-            color: "#32325d"
-          }
-        },
-        invalid: {
-          color: "#fa755a",
-          iconColor: "#fa755a"
-        }
-      }
-    };
-    const handleChange = async (event) => {
-      // Listen for changes in the CardElement
-      // and display any errors as the customer types their card details
-      setDisabled(event.empty);
-      setError(event.error ? event.error.message : "");
-    };
-    const handleSubmit = async ev => {
-      ev.preventDefault();
-      setProcessing(true);
-      const payload = await stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-          card: elements.getElement(CardElement),
-          billing_details: {
-            name: ev.target.name.value
-          }
-        }
-      });
-      if (payload.error) {
-        setError(`Payment failed ${payload.error.message}`);
-        setProcessing(false);
-      } else {
-        setError(null);
-        setProcessing(false);
-        setSucceeded(true);
-      }
-    };
-    return (
-      <form id="payment-form" onSubmit={handleSubmit}>
-        <CardElement id="card-element" options={cardStyle} onChange={handleChange} />
-        <button
-          disabled={processing || disabled || succeeded}
-          id="submit"
-        >
-          <span id="button-text">
-            {processing ? (
-              <div className="spinner" id="spinner"></div>
-            ) : (
-              "Pay"
-            )}
-          </span>
-        </button>
-        {/* Show any error that happens when processing the payment */}
-        {error && (
-          <div className="card-error" role="alert">
-            {error}
-          </div>
-        )}
-        {/* Show a success message upon completion */}
-        <p className={succeeded ? "result-message" : "result-message hidden"}>
-          Payment succeeded, see the result in your
-          <a
-            href={`https://dashboard.stripe.com/test/payments`}
-          >
-            {" "}
-            Stripe dashboard.
-          </a> Refresh the page to pay again.
-        </p>
-      </form>
-    );
-  }
-
+export default PayMentPage
 
 
