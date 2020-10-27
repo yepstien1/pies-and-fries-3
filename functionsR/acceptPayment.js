3b74d0d7c6c06326cd47139f7552a980289311ad
Jacob E <yaakovepstein@gmail.com>
2020-10-27 15:40:25 -0400

    trying to log out session object - just the "object"

--- functionsR/acceptPayment.js
+++ functionsR/acceptPayment.js
@@ -30,9 +30,9 @@ exports.handler = async (event, context) => {
         });
     console.log("session with stringify:" + JSON.stringify(session))
     console.log("session withput stringify:" + session)
-    console.log("object:" + JSON.stringify(session.object.checkout.session))
+    console.log("object:" + JSON.stringify(session.object))
 
-    console.log("checkout session:" + JSON.stringify(session.object.checkout.session))
+    console.log("checkout session:" + JSON.stringify(session.object))
 
 
 }
