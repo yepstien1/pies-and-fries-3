7a9a53166d5f7fc1bc681833843ab3b8e14dae01
Jacob E <yaakovepstein@gmail.com>
2020-11-03 14:40:47 -0500

    trying to pass via json

--- functionsR/acceptPayment.js
+++ functionsR/acceptPayment.js
@@ -23,7 +23,7 @@ exports.handler = async (event, context) => {
             },
         ],
         mode: 'payment',
-        success_url: `https://pies-and-fries.netlify.app?info=${info}`,
+        success_url: `https://pies-and-fries.netlify.app?info=${event.body}`,
         cancel_url: 'https://example.com/cancel',
     });
 console.log("hi from func")
